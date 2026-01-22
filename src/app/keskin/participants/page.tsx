5f83e5088e4edb4cf9d3068ec917d7f08d4ba3af
Esra Aydemir <es.altintas@gmail.com>
2026-01-23 02:10:14 +0300

    Removed keskin

--- src/app/keskin/participants/page.tsx
+++
@@ -1,5 +0,0 @@
-import ParticipantsClient from "@/components/ParticipantsClient";
-
-export default function KeskinParticipantsPage() {
-  return <ParticipantsClient mode="keskin" />;
-}
--- src/app/keskin/stages/[stageId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import StageRankingClient from "@/components/StageRankingClient";
-
-export default function Page({ params }: { params: { stageId: string } }) {
-  return <StageRankingClient mode="keskin" stageId={params.stageId} />;
-}
